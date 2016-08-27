bf804f4857efb11822ab4ec3d8d1962672431160
2998550097 <2998550097@qq.com>
2016-08-27 18:35:12 +0800

    我在Test.java增加了name属性

--- Myvote/src/main/java/com/yc/vote/entity/Test.java
+++ Myvote/src/main/java/com/yc/vote/entity/Test.java
@@ -2,6 +2,7 @@ package com.yc.vote.entity;
 
 public class Test {
 	private int number;
+	private String name;
 
 	public int getNumber() {
 		return number;
@@ -9,6 +10,14 @@ public class Test {
 
 	public void setNumber(int number) {
 		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
