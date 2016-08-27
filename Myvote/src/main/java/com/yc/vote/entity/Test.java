7455626da2e869c3d659a5b46bdb5b3d3cd4b329
xiangshang190823 <1041975591@qq.com>
2016-08-27 17:02:20 +0800

    这是一个测试实体类test.java

---
+++ Myvote/src/main/java/com/yc/vote/entity/Test.java
@@ -0,0 +1,15 @@
+package com.yc.vote.entity;
+
+public class Test {
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
