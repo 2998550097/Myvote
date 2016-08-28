75d0a059deb6fa33959ff38dfdfdb02e07464485
Gloria18 <158443412@qq.com>
2016-08-28 09:13:52 +0800

    添加了一个Test类并在里面添加了name属性。

---
+++ Myvote/src/test/java/com/yc/connTest/Test.java
@@ -0,0 +1,15 @@
+package com.yc.connTest;
+
+public class Test {
+	private String name ;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
