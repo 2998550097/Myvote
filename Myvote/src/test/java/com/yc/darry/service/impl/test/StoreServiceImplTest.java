885599a2a328a0e87e297af3993b284f98b678b7
xiangshang190823 <1041975591@qq.com>
2016-09-03 11:30:07 +0800

    店铺测试类提交--向尚

---
+++ Myvote/src/test/java/com/yc/darry/service/impl/test/StoreServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.yc.darry.service.impl.test;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yc.darry.entity.Store;
+import com.yc.darry.service.StoreService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class StoreServiceImplTest {
+
+	@Autowired
+	private StoreService storeService;
+	@Test
+	public void testAddStore() {
+		Store  store=new Store( "a", "a", 0,"a", "a", "a");
+		System.out.println(storeService.addStore(store));
+	}
+
+}
